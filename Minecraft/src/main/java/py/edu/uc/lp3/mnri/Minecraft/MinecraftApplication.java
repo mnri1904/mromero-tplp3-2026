9e52c4db69e44d01392be8a02d2288faa2e70d25
mnri1904 <marcenicoromero@gmail.com>
2026-09-16 14:38:57 -0300

    chore: agrega proyecto Minecraft descomprimido

---
+++ Minecraft/src/main/java/py/edu/uc/lp3/mnri/Minecraft/MinecraftApplication.java
@@ -0,0 +1,13 @@
+package py.edu.uc.lp3.mnri.Minecraft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinecraftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinecraftApplication.class, args);
+	}
+
+}
---
+++ Minecraft/src/test/java/py/edu/uc/lp3/mnri/Minecraft/MinecraftApplicationTests.java
@@ -0,0 +1,13 @@
+package py.edu.uc.lp3.mnri.Minecraft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinecraftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
